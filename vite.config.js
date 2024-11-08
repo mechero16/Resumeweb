b9113ae0eb66386e471b83ad560f7fc878999e9a
mechero16 <harshmcs16@gmail.com>
2024-11-08 10:27:57 +0530

    Initial5 commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/mechero16/Resumeweb.git", // Replace 'your-repo-name' with your actual GitHub repository name
+  base: "https://github.com/mechero16/Resumeweb", // Replace 'your-repo-name' with your GitHub repository name
 });
